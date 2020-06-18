2295b4e6e7ed9812c9a49ace6d0b362700543735
Wim Spaargaren <wim@weave.nl>
2020-06-18 15:39:42 +0200

    Bug where plugin got null pointer for imported projects

--- popup/page_helper.js
+++ popup/page_helper.js
@@ -70,13 +70,14 @@ function setCurProject() {
     let searchQueryField = ByID(document, "searchQuery")
     searchQueryField.value = curProject.search_string
     let projectScrapingState = JSON.parse(atob(curProject.scrape_state))
-    document.getElementById('queryWebOfScience').value = projectScrapingState.urls[0]
-    document.getElementById('queryACM').value = projectScrapingState.urls[5]
-    document.getElementById('querySpringer').value = projectScrapingState.urls[2]
-    document.getElementById('queryIEEE').value = projectScrapingState.urls[4]
-    document.getElementById('queryScholar').value = projectScrapingState.urls[3]
-    document.getElementById('queryScienceDirect').value = projectScrapingState.urls[1]
-
+    if (projectScrapingState.urls && projectScrapingState.urls.length > 0) {
+        document.getElementById('queryWebOfScience').value = projectScrapingState.urls[0]
+        document.getElementById('queryACM').value = projectScrapingState.urls[5]
+        document.getElementById('querySpringer').value = projectScrapingState.urls[2]
+        document.getElementById('queryIEEE').value = projectScrapingState.urls[4]
+        document.getElementById('queryScholar').value = projectScrapingState.urls[3]
+        document.getElementById('queryScienceDirect').value = projectScrapingState.urls[1]
+    }
     let projectDisplayContainers = ByClassName(document, "cur-project-name")
     for (let i = 0; i < projectDisplayContainers.length; i++) {
         projectDisplayContainers[i].innerHTML = curProject.name
@@ -89,10 +90,10 @@ function setCurProject() {
     for (let i = 0; i < projectArticleCountDisplayContainers.length; i++) {
         projectArticleCountDisplayContainers[i].innerHTML = curProject.amount_of_articles
     }
-    if(curProject.status > enums.ProjectStatusConductingSearch){
+    if (curProject.status > enums.ProjectStatusConductingSearch) {
         ByID(document, "article-info-manager").style.display = "block";
         ByID(document, "scrape-manager").style.display = "none";
-    }else{
+    } else {
         ByID(document, "article-info-manager").style.display = "none";
         ByID(document, "scrape-manager").style.display = "block";
     }
